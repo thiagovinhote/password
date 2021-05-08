e1fa4ad6f94510cf91cc472eb028e3e26793b61a
Vinhote <thiago.esw@gmail.com>
2021-05-08 01:52:39 -0300

    refactor: change select color

--- src/presentation/pages/generate/select-size.tsx
+++ src/presentation/pages/generate/select-size.tsx
@@ -15,9 +15,9 @@ export const SelectSize: React.FC<Props> = ({ data, value, onChange }) => {
       {({ open }) => (
         <Fragment>
           <div className="relative">
-            <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+            <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
               <span className="flex items-center">
-                <span className="ml-3 block truncate">{value.label}</span>
+                <span className="ml-1 block truncate">{value.label}</span>
               </span>
               <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                 <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -38,7 +38,7 @@ export const SelectSize: React.FC<Props> = ({ data, value, onChange }) => {
                 {data.map((item) => (
                   <Listbox.Option
                     key={item.value}
-                    className={({ active }) => classNames(active ? 'text-white bg-indigo-600' : 'text-gray-900', 'cursor-default select-none relative py-2 pl-3 pr-9')}
+                    className={({ active }) => classNames(active ? 'text-white bg-blue-600' : 'text-gray-800', 'cursor-default select-none relative py-2 pl-1 pr-9')}
                     value={item}
                   >
                     {({ selected, active }) => (
@@ -50,7 +50,7 @@ export const SelectSize: React.FC<Props> = ({ data, value, onChange }) => {
                         </div>
 
                         {selected && (
-                          <span className={classNames(active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4')}>
+                          <span className={classNames(active ? 'text-white' : 'text-blue-600', 'absolute inset-y-0 right-0 flex items-center pr-4')}>
                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
                           </span>
                         )}
