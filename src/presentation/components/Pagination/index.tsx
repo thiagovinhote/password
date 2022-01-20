0ea0552b56a44b37587a01a1d1cba6698ae623d5
Vinhote <thiago.esw@gmail.com>
2022-01-19 23:56:31 -0400

    refactor: improvement pagination component

--- src/presentation/components/Pagination/index.tsx
+++ src/presentation/components/Pagination/index.tsx
@@ -2,20 +2,32 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 import React from 'react'
 import Link from 'next/link'
 import { PaginatorTypes } from '~/domain/models/paginator'
-import { classNames, range } from '~/presentation/helpers'
+import { classNames } from '~/presentation/helpers'
+import { DOTS, usePagination } from './use-pagination'
+import { useRouter } from 'next/router'
 
 type Props = {
   value: PaginatorTypes.Pagination
 }
 
 export const Pagination: React.FC<Props> = props => {
-  const right = Math.min(
-    props.value.currentPage * props.value.perPage,
-    props.value.total
-  )
-  const left = right - Math.min(props.value.perPage, props.value.total) + 1
-  const items = range(1, props.value.lastPage + 1)
+  const router = useRouter()
 
+  const paginationRange = usePagination({
+    currentPage: props.value.currentPage,
+    totalCount: props.value.total,
+    siblingCount: 1,
+    pageSize: props.value.perPage
+  })
+
+  // If there are less than 2 times in pagination range we shall not render the component
+  if (props.value.currentPage === 0 || paginationRange.length < 2) {
+    return null
+  }
+
+  const lastPage = paginationRange[paginationRange.length - 1]
+  const previousIsDisabled = props.value.currentPage === 1
+  const nextIsDisabled = props.value.currentPage === lastPage
   const currentStyle = 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
   const defaultStyle = 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50'
 
@@ -38,11 +50,8 @@ export const Pagination: React.FC<Props> = props => {
       <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
-            Mostrando&nbsp;
-            <span className="font-medium">{left}&nbsp;</span>a&nbsp;
-            <span className="font-medium">{right}&nbsp;</span>de&nbsp;
             <span className="font-medium">{props.value.total}&nbsp;</span>
-            resultados
+            resultados encontrados
           </p>
         </div>
         <div>
@@ -50,28 +59,57 @@ export const Pagination: React.FC<Props> = props => {
             className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
             aria-label="Pagination"
           >
-            <a
-              href="#"
-              className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+            <Link
+              passHref
+              href={{
+                query: !previousIsDisabled
+                  ? { ...router.query, page: props.value.currentPage - 1 }
+                  : null
+              }}
             >
-              <span className="sr-only">Anterior</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+              <a
+                className={classNames(
+                  'relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium hover:bg-gray-50',
+                  previousIsDisabled
+                    ? 'text-gray-200 pointer-events-none'
+                    : 'text-gray-500'
+                )}
+                aria-disabled
+              >
+                <span className="sr-only">Anterior</span>
+                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+              </a>
+            </Link>
+
             {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
-            {items.map(page => (
-              <Link passHref key={page} href={{ query: { page } }}>
-                <a
-                  className={classNames(
-                    'relative inline-flex items-center px-4 py-2 border text-sm font-medium',
-                    page === props.value.currentPage
-                      ? currentStyle
-                      : defaultStyle
-                  )}
+            {paginationRange.map(pageNumber => {
+              if (pageNumber === DOTS) {
+                return (
+                  <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
+                    &#8230;
+                  </span>
+                )
+              }
+
+              return (
+                <Link
+                  passHref
+                  key={pageNumber}
+                  href={{ query: { ...router.query, page: pageNumber } }}
                 >
-                  {page}
-                </a>
-              </Link>
-            ))}
+                  <a
+                    className={classNames(
+                      'relative inline-flex items-center px-4 py-2 border text-sm font-medium',
+                      pageNumber === props.value.currentPage
+                        ? currentStyle
+                        : defaultStyle
+                    )}
+                  >
+                    {pageNumber}
+                  </a>
+                </Link>
+              )
+            })}
 
             {/* <a
               href="#"
@@ -85,13 +123,27 @@ export const Pagination: React.FC<Props> = props => {
               ...
             </span>
              */}
-            <a
-              href="#"
-              className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+            <Link
+              passHref
+              href={{
+                query: !nextIsDisabled
+                  ? { ...router.query, page: props.value.currentPage + 1 }
+                  : null
+              }}
             >
-              <span className="sr-only">Próximo</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+              <a
+                className={classNames(
+                  'relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium hover:bg-gray-50',
+                  nextIsDisabled
+                    ? 'text-gray-200 pointer-events-none'
+                    : 'text-gray-500'
+                )}
+                aria-disabled
+              >
+                <span className="sr-only">Próximo</span>
+                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+              </a>
+            </Link>
           </nav>
         </div>
       </div>
