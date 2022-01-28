9f7c74a0c8988c755f8b808d488075e76ac9226a
Vinhote <thiago.esw@gmail.com>
2022-01-27 23:33:46 -0400

    feat: add makeApiDeleteCredential factory

---
+++ src/main/factories/usecases/api-delete-credential.ts
@@ -0,0 +1,9 @@
+import { parseCookies } from 'nookies'
+import { ApiDeleteCredential } from '~/data/usecases/api-delete-credential'
+import { makeAuthorizationHttpClient } from '../decorators'
+
+export const makeApiDeleteCredential = (
+  mapCookies = parseCookies()
+): ApiDeleteCredential => {
+  return new ApiDeleteCredential(makeAuthorizationHttpClient(mapCookies))
+}
