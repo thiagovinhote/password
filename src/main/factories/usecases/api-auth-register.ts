4b333b441830853a27771a1aec1ba95b347d0f7f
Vinhote <thiago.esw@gmail.com>
2022-01-30 16:52:54 -0400

    feat: export api-auth-register factory

---
+++ src/main/factories/usecases/api-auth-register.ts
@@ -0,0 +1,7 @@
+import { ApiAuthRegister } from '~/data/usecases/api-auth-register'
+import { FetchHttpClient } from '~/infra/http-client/fetch-http-client'
+import { makeBaseApi } from '../http/base-api'
+
+export const makeApiAuthRegister = (): ApiAuthRegister => {
+  return new ApiAuthRegister(new FetchHttpClient(makeBaseApi()))
+}
