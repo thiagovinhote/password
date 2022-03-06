073eca6325b71621ba759f36c7493c199d87900a
Vinhote <thiago.esw@gmail.com>
2022-03-06 17:51:52 -0300

    feat: create helpers to serializer classes

---
+++ src/domain/fields/plain-object.ts
@@ -0,0 +1,45 @@
+type Unpacked<T> = T extends (infer U)[] ? U : T
+type Packed<T, U> = T extends ReadonlyRequired<any[]>
+  ? U[]
+  : T extends ReadonlyRequired<infer X>
+  ? X
+  : T
+type TypeName<T> = T extends Date ? string : T
+export type ReadonlyRequired<T> = { readonly [P in keyof T]-?: TypeName<T[P]> }
+
+export class PlainObject {
+  static serializer<Instance, Value = Unpacked<Instance>>(
+    instance: Instance
+  ): ReadonlyRequired<Value> {
+    if (Array.isArray(instance)) {
+      return (instance.map(
+        PlainObject.serializer
+      ) as unknown) as ReadonlyRequired<Value>
+    }
+
+    const newEntries = Object.entries(instance).map(([key, value]) => {
+      if (value === undefined) {
+        return [key, null]
+      }
+      if (value instanceof Date) {
+        return [key, value.toISOString()]
+      }
+      if (Array.isArray(value)) {
+        return [key, value.map(PlainObject.serializer)]
+      }
+      return [key, value]
+    })
+    return Object.fromEntries(newEntries)
+  }
+
+  static deserializer<
+    T extends (...args: any) => any,
+    Instance,
+    Value = Packed<Instance, ReturnType<T>>
+  >(factory: T, value: Instance): Value {
+    if (Array.isArray(value)) {
+      return (value.map(factory) as unknown) as Value
+    }
+    return factory(value)
+  }
+}
