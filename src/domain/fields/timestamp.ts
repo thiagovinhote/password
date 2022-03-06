3fdcfb618682892f8d5735e291dc5a8622e2639d
Vinhote <thiago.esw@gmail.com>
2022-03-06 17:50:54 -0300

    feat: create Timestamp class

---
+++ src/domain/fields/timestamp.ts
@@ -0,0 +1,11 @@
+export class Timestamp extends Date {
+  static create(value?: string | Date): Timestamp | null {
+    if (value instanceof Date) {
+      return new Timestamp(value)
+    } else if (!value?.trim()) {
+      return null
+    } else {
+      return new Timestamp(value)
+    }
+  }
+}
