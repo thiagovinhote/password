65ef25376d565735d02e398fb7c1bacb6bacbf2e
Vinhote <thiago.esw@gmail.com>
2022-07-14 22:53:45 -0300

    fix: run lint to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
     extend: {
       backgroundColor: ['active']
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
